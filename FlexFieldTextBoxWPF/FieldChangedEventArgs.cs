ac2346e9ddbafeac6bede282af1f415c9dfa1877
John Baughman <JohnMBaughman@gmail.com>
2023-08-07 21:21:45 -0500

    the beginning

---
+++ FlexFieldTextBoxWPF/FieldChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace FlexFieldTextBoxWPF;
+
+public class FieldChangedEventArgs
+{
+    /// <summary>
+    /// Gets or sets index of field that is raising FieldChangedEvent.
+    /// </summary>
+    public int FieldIndex { get; set; }
+
+    /// <summary>
+    /// Gets or sets text of field that is raising FieldChangedEvent.
+    /// </summary>
+    public string Text { get; set; }
+}
